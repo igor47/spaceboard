6ce3bb9864adfafa54724abe9a5d12b4dff564e1
Igor Serebryany <igor47@moomers.org>
2017-09-28 23:37:38 +0000

    fix comms with server
    still encountering problems where all the leds turn white...

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,9 @@
 #define LED_STRIP_PIN 14  // we chose a pin on GPIO port C in case we switch to DMA
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(20, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-static const uint32_t StateSendMS = 500; // ms
-static const uint32_t BaudRate = 57600;  // baud
+//static const uint32_t BaudRate = 57600;  // baud
+static const uint32_t BaudRate = 9600;  // baud
+static uint32_t loopTime = 0;
 
 /*************** Data Types  *********************/
 struct SerialCommand {
@@ -33,7 +34,7 @@ struct SerialCommand {
   SerialCommand(Type t) : type(t), ledNum(0), red(0), green(0), blue(0) { }
 
   Type type;
-  uint8_t ledNum;
+  uint16_t ledNum;
   uint8_t red;
   uint8_t green;
   uint8_t blue;
@@ -54,7 +55,7 @@ static struct State {
 
 /*************** Prototypes  *********************/
 
-void send_state(unsigned long now);
+void send_state();
 SerialCommand read_server_command();
 SerialCommand parse_command_buffer(const char* buf, int len);
 void execute_command(const SerialCommand& cmd);
@@ -70,6 +71,7 @@ void setup()
 
   // initialize serial
   Serial3.begin(BaudRate);
+  Serial3.print("boot!\r\n");
 
   // initialize leds
   strip.begin();
@@ -79,40 +81,32 @@ void setup()
 void loop()
 {
   // get the current time
-  unsigned long now = millis();
+  loopTime = millis();
 
   // if time overflows, reset last command timestamp to now instead of
   // having a ridiculous overflow. this happens every 50 days...
-  if (state.lastCommandTimestamp > now)
-    state.lastCommandTimestamp = now;
+  if (state.lastCommandTimestamp > loopTime)
+    state.lastCommandTimestamp = loopTime;
 
   // communicate with the server
-  send_state(now);
   SerialCommand cmd = read_server_command();
-
-  if (cmd.type == SerialCommand::NONE ||
-      cmd.type == SerialCommand::BAD) {
+  if (cmd.type == SerialCommand::NONE ) {
+    // nothing to do
+  } else if (cmd.type == SerialCommand::BAD) {
     // log receiving bad commands
-    if (cmd.type == SerialCommand::BAD) {
-      state.badCommandsReceived++;
-    }
+    state.badCommandsReceived++;
 
   // got a good command from the server; log and execute
   } else {
-    state.lastCommandTimestamp = now;
+    state.lastCommandTimestamp = loopTime;
     state.commandsReceived++;
     execute_command(cmd);
   }
 }
 
 // sends the current program state to the computer
-void send_state(unsigned long now)
+void send_state()
 {
-  if (state.lastStateSentTimestamp != 0 &&
-      now - state.lastStateSentTimestamp < StateSendMS) {
-    return;
-  }
-
   Serial3.print("C:");
   Serial3.print(state.commandsReceived, DEC);
   Serial3.print(";");
@@ -122,12 +116,12 @@ void send_state(unsigned long now)
   Serial3.print(";");
 
   Serial3.print("L:");
-  Serial3.print(now - state.lastCommandTimestamp, DEC);
+  Serial3.print(loopTime - state.lastCommandTimestamp, DEC);
   Serial3.print(";");
 
   Serial3.print("\r\n");
 
-  state.lastStateSentTimestamp = now;
+  state.lastStateSentTimestamp = millis();
   toggle_led();
 }
 
@@ -138,10 +132,11 @@ SerialCommand read_server_command()
   static char buf[20];
   static int buf_pos = 0;
   static bool buffer_overflow = false;
-  char c;
 
   // read data from serial port and write it into buf
-  while ((c = Serial.read()) != -1) {
+  char c;
+  while (Serial3.available()) {
+    c = Serial3.read();
     buf[buf_pos] = c;
 
     // we've read a whole command once we see a '\n'
@@ -205,15 +200,15 @@ SerialCommand parse_command_buffer(const char* buf, int len)
     if (*buf != ',' || tmpN > UCHAR_MAX) {
       cmd.type = SerialCommand::BAD;
     } else {
-      cmd.ledNum = tmpN;
+      cmd.ledNum = (uint16_t)tmpN;
 
       buf = scan_int(buf+1, &tmpN);
-      if (*buf != '\n') {
+      if (*buf != '\n' && *buf != '\r') {
         cmd.type = SerialCommand::BAD;
       } else {
-        cmd.blue  = (tmpN >> 0) & 255;
-        cmd.green = (tmpN >> 8) & 255;
-        cmd.red   = (tmpN >> 16) & 255;
+        cmd.blue  = (uint8_t)(tmpN >> 0);
+        cmd.green = (uint8_t)(tmpN >> 8);
+        cmd.red   = (uint8_t)(tmpN >> 16);
       }
     }
     break;
@@ -228,7 +223,7 @@ void execute_command(const SerialCommand& cmd)
 {
   // send state now (well, next loop)
   if (cmd.type == SerialCommand::GETSTATE) {
-    state.lastStateSentTimestamp = 0;
+    send_state();
   }
 
   // set a particular led to a given color
@@ -242,8 +237,9 @@ void execute_command(const SerialCommand& cmd)
     for(uint16_t i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(i, 0, 0, 0);
     }
-    strip.show();
   }
+
+  strip.show();
 }
 
 void toggle_led()
