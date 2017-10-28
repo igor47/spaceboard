5ad1a43a9c438e778b042369fc80b7e74e555bca
igor <igor47@moomers.org>
2017-10-27 19:40:30 -0700

    client: correct (new) protocol
    this uses length prefixing instead of cobs

--- spaceteam/client.py
+++ spaceteam/client.py
@@ -3,9 +3,9 @@
 Acts as a client to a central control server
 """
 
-from cobs import cobs
 import json
 import socket
+import struct
 
 DEFAULT_PORT = 8000
 
@@ -15,12 +15,14 @@ class Client:
   def __init__(self):
     """initialize comms"""
     self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self._socket.setblocking(0)
-
-    self.read_buffer = []
+    self.read_buffer = ""
 
   def connect(self, host, port = DEFAULT_PORT):
     self._socket.connect((host, port))
+    self._socket.setblocking(0)
+
+  def stop(self):
+    self._socket.close()
 
   def send(self, message, data):
     """encodes and sends a message to the server"""
@@ -28,33 +30,42 @@ class Client:
       'message': message,
       'data': data,
       })
-
-    self._socket.sendall(msg + '\x00')
+    self._socket.sendall(msg)
 
   def read(self):
     """returns a message from server, or None"""
+    no_data = False
     while True:
-      new_data = self._socket.recv(4096)
+      try:
+        self.read_buffer += self._socket.recv(4096)
+      except socket.error:
+        no_data = True
 
-      # stop trying to read if we're not getting anything
-      if len(new_data) == 0:
-        return None
+      msg = self.pop_from_buffer()
+      if msg:
+        return msg
+      elif no_data:
+        break
 
-      # save what we just got
-      self.read_buffer.append(new_data)
+  def encode(self, msg):
+    """encodes a message to send to the server"""
+    jstr = json.dumps(msg)
+    return struct.pack('>I%ds' % len(jstr), len(jstr), jstr)
 
-      # do we have a full command? lets return it
-      if '\x00' in new_data:
-        all_read = ''.join(self.read_buffer)
-        msg, remainder = all_read.split('\x00')
-        self.read_buffer = [remainder]
+  def pop_from_buffer(self):
+    """parses a message read from the buffer"""
+    # do we even know the length of the next message?
+    if len(self.read_buffer) < 4:
+      return None
 
-        return decode(msg)
+    # have we received the entire next message?
+    msg_length = struct.unpack('>I', self.read_buffer[:4])[0]
+    if len(self.read_buffer) < (4 + msg_length):
+      return None
 
-    def encode(self, msg):
-      """encodes a message to send to the server"""
-      return cobs.encode(json.dumps(msg))
+    # okay, we have a complete message! lets return it!
+    msg_ends_at = 4 + msg_length
+    msg = self.read_buffer[4:msg_ends_at]
+    self.read_buffer = self.read_buffer[msg_ends_at:]
 
-    def decode(self, msg):
-      """parses a message read from the socket"""
-      return json.loads(cobs.decode(msg))
+    return json.loads(msg)
