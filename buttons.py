2384a067e85ad0c658c45e9118e5ac12d58e666a
Igor Serebryany <igor47@moomers.org>
2017-11-01 08:58:34 +0000

    buttons: easier output

--- buttons.py
+++ buttons.py
@@ -5,9 +5,15 @@ peripherals.reset_all()
 
 import time
 
-m = peripherals.MCP26
+m = peripherals.MCP25
+m.communicate()
+prev = m.input_latches
+
 while True:
   m.communicate()
-  print m.input_latches
+  for idx, val in enumerate(m.input_latches):
+    if prev[idx] != val:
+      print "val %d has changed from %d to %d" % (idx, prev[idx], val)
 
+  prev = m.input_latches
   time.sleep(0.1)
