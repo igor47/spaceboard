e0240799f15f9b11ac987d681669d56ab472b807
igor <igor47@moomers.org>
2018-04-16 13:25:19 -0700

    display testing code

---
+++ display.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+
+from spaceteam import peripherals
+peripherals.toggle_reset()
+peripherals.DISPLAY.reset()
+
+import time
+
+while True:
+  peripherals.DISPLAY.message = 'Unix time is %s' % time.time()
+  peripherals.DISPLAY.communicate()
+  time.sleep(1)
